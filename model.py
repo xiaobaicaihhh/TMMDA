eeaa1738462163e233bb292f34dc38a5ecd51768
zhaoxianbing <37646484+xiaobaicaihhh@users.noreply.github.com>
2022-12-20 21:19:59 +0800

    Update model.py
    update multimodal information

--- model.py
+++ model.py
@@ -9,20 +9,44 @@ class TMMDA(nn.Module):
         # self.discriminator = nn.Sequential(MAGPooler(dim=TEXT_DIM), nn.Linear(TEXT_DIM, 2))
         self.mixup = TokenMixUp()
     
-        self.generator = nn.Sequential(
+        self.generator_l = nn.Sequential(
             nn.Linear(TEXT_DIM, TEXT_DIM),
             nn.Dropout(p=0.1),
             nn.Tanh(),
             nn.Linear(TEXT_DIM, TEXT_DIM)
         ) 
-
-        self.discriminator = nn.Sequential(
+        self.discriminator_l = nn.Sequential(
             nn.Linear(TEXT_DIM, TEXT_DIM // 2),
             nn.Dropout(p=0.1),
             nn.ReLU(),
             nn.Linear(TEXT_DIM // 2, 2),
         )
 
+        self.generator_v = nn.Sequential(
+            nn.Linear(TEXT_DIM, TEXT_DIM),
+            nn.Dropout(p=0.1),
+            nn.Tanh(),
+            nn.Linear(TEXT_DIM, TEXT_DIM)
+        ) 
+        self.discriminator_v = nn.Sequential(
+            nn.Linear(TEXT_DIM, TEXT_DIM // 2),
+            nn.Dropout(p=0.1),
+            nn.ReLU(),
+            nn.Linear(TEXT_DIM // 2, 2),
+        )
+
+        self.generator_a = nn.Sequential(
+            nn.Linear(TEXT_DIM, TEXT_DIM),
+            nn.Dropout(p=0.1),
+            nn.Tanh(),
+            nn.Linear(TEXT_DIM, TEXT_DIM)
+        ) 
+        self.discriminator_a = nn.Sequential(
+            nn.Linear(TEXT_DIM, TEXT_DIM // 2),
+            nn.Dropout(p=0.1),
+            nn.ReLU(),
+            nn.Linear(TEXT_DIM // 2, 2),
+        )
         
         self.adv_loss = nn.CrossEntropyLoss()
         self.js_loss = JSD()
@@ -33,43 +57,82 @@ class TMMDA(nn.Module):
         self.trans_v = nn.TransformerEncoder(encoder_v, num_layers=3)
         
 
-        self.attn = MultiheadAttention(embed_dim=TEXT_DIM, num_heads=8, attn_dropout=0.1)
+        self.attn1 = MultiheadAttention(embed_dim=TEXT_DIM, num_heads=8, attn_dropout=0.1)
+        self.attn2 = MultiheadAttention(embed_dim=TEXT_DIM, num_heads=8, attn_dropout=0.1)
+        self.attn3 = MultiheadAttention(embed_dim=TEXT_DIM, num_heads=8, attn_dropout=0.1)
         
-        # self.pooler_l = MAGPooler(dim=TEXT_DIM)
-        # self.pooler_a = MAGPooler(dim=TEXT_DIM)
-        # self.pooler_v = MAGPooler(dim=TEXT_DIM)
-        self.fc_lm = nn.Linear(2*TEXT_DIM, TEXT_DIM)
+        self.trans = TransformerEncoder(embed_dim=TEXT_DIM, num_heads=8, layers=1)
+        
     def forward(self, text_embedding, visual, acoustic, input_mask_mix=None, training=True):
         bsz, seq, dim = text_embedding.shape
         #torch.Size([48, 50, 768]) torch.Size([48, 50, 47]) torch.Size([48, 50, 74])
         xl, xv, xa = text_embedding, visual, acoustic
         xv, xa = self.fc2(xv), self.fc1(xa)
+        
+
+        
         # add gan
         mask = torch.rand(bsz, seq).to(DEVICE)
-        xlm = self.mixup(xl, xv, xa, mask, input_mask_mix)
-        #training noise gen
+        xlm = self.mixup(xl, xv, xa, mask, input_mask_mix, prob1=rho_1, prob2=rho_2)
+        xvm = self.mixup(xv, xl, xa, mask, input_mask_mix, prob1=rho_1, prob2=rho_2)
+        xam = self.mixup(xa, xv, xa, mask, input_mask_mix, prob1=rho_1, prob2=rho_2)
         if training:
-            xm_noise = alpha_noise*torch.randn(bsz, seq, dim).to(DEVICE) #+ xm_noise
-            xlmg = self.generator(xlm) + xm_noise
+            xlm_noise = alpha_noise*torch.randn(bsz, seq, dim).to(DEVICE) #+ xm_noise
+            xlmg = self.generator_l(xlm) + xlm_noise
+
+            xvm_noise = alpha_noise*torch.randn(bsz, seq, dim).to(DEVICE) #+ xm_noise
+            xvmg = self.generator_v(xvm) + xvm_noise
+
+            xam_noise = alpha_noise*torch.randn(bsz, seq, dim).to(DEVICE) #+ xm_noise
+            xamg = self.generator_a(xam) + xam_noise            
         else:
-            xlmg = self.generator(xlm)
-        # discrimilator
-        xdl = self.discriminator(xl)
-        xdm = self.discriminator(xlmg)
-        # label
+            xlmg = self.generator_l(xlm)
+            xvmg = self.generator_v(xvm)
+            xamg = self.generator_a(xam)
+            
+        xdl = self.discriminator_l(xl)
+        xdlmg = self.discriminator_l(xlmg)
+        
+        xdv = self.discriminator_v(xv)
+        xdvmg = self.discriminator_v(xvmg)
+        
+        xda = self.discriminator_a(xa)
+        xdamg = self.discriminator_a(xamg)
+                
         text_label_real = torch.ones(bsz*50, dtype=torch.long).to(DEVICE)
         text_label_fake = torch.zeros(bsz*50, dtype=torch.long).to(DEVICE)
-        # loss
-        adv_loss = self.adv_loss(xdl.view(-1, 2), text_label_real) + self.adv_loss(xdm.view(-1, 2), text_label_fake)
-        js_loss = self.js_loss(xl, xlm)
-
+        
+        adv_loss_l = self.adv_loss(xdl.view(-1, 2), text_label_real) + self.adv_loss(xdlmg.view(-1, 2), text_label_fake)
+        adv_loss_v = self.adv_loss(xdv.view(-1, 2), text_label_real) + self.adv_loss(xdvmg.view(-1, 2), text_label_fake)
+        adv_loss_a = self.adv_loss(xda.view(-1, 2), text_label_real) + self.adv_loss(xdamg.view(-1, 2), text_label_fake)
+        adv_loss = (adv_loss_l +  adv_loss_v + adv_loss_a) / 3
+        
+        js_loss_l = self.js_loss(xl, xlmg)
+        js_loss_v = self.js_loss(xv, xvmg)
+        js_loss_a = self.js_loss(xa, xamg)
+        js_loss = (js_loss_l + js_loss_v + js_loss_a) / 3
         # transformer encoder
-        xl, xlmg = xl.transpose(0, 1), xlmg.transpose(0, 1)
-        xl = self.attn(xl, xlmg, xlmg)[0] + xl
-        #xl = self.fc_lm(torch.cat([xclmg, xl], dim=-1))
-        xl = xl.transpose(0, 1)
-        return xl, adv_loss, js_loss
+        #ret_xl, ret_xlmg, ret_xv, ret_xvmg, ret_xa, ret_xamg = xl[:, 0, :], xlmg[:, 0, :], xv[:,0,:], xvmg[:,0,:], xa[:,0,:], xamg[:,0,:]
+        ret_xl, ret_xlmg, ret_xv, ret_xvmg, ret_xa, ret_xamg = torch.sum(xl, dim=-1), torch.sum(xlmg, dim=-1), torch.sum(xv, dim=-1), torch.sum(xvmg, dim=-1), torch.sum(xa, dim=-1), torch.sum(xamg, dim=-1)
 
+        xl, xlmg = xl.transpose(0, 1), xlmg.transpose(0, 1)
+        xl = self.attn1(xl, xlmg, xlmg)[0] + xl + text_embedding.transpose(0, 1)
+        xl = xl.transpose(0, 1)
+        
+        xv, xvmg = xv.transpose(0, 1), xvmg.transpose(0, 1)
+        xv = self.attn2(xv, xvmg, xvmg)[0] + xv
+        xv = xv.transpose(0, 1)
+        
+        xa, xamg = xa.transpose(0, 1), xamg.transpose(0, 1)
+        xa = self.attn3(xa, xamg, xamg)[0] + xa
+        xa = xa.transpose(0, 1)
+
+        x = torch.cat([xl, xv, xa], dim=1)
+        x = x.transpose(0, 1)
+        x = self.trans(x)
+        x = x.transpose(0, 1)
+        return x
+    
 class JSD(nn.Module):
     def __init__(self):
         super(JSD, self).__init__()
